9d543714e4ec5f1013f2a6637146f6fbe6aa692a
Isaaaca <isaaaca@gmail.com>
2020-03-16 15:36:05 +0800

    Added Quest 1

--- World/Apple_Orchard/Prophet.py
+++ World/Apple_Orchard/Prophet.py
@@ -3,11 +3,11 @@
 '''
 
 from Util import *
-import os
+import os, importlib
 import ProphetState as State
 
 
-Type = 'person'
+isPerson = True
 CurrentLocation = os.path.dirname(__file__)
 
 def talk():
@@ -16,27 +16,45 @@ def talk():
         quest1()
 
 def quest1():
-    wrapper = textwrap.TextWrapper()
-    wrapper.initial_indent = "* "
-    print('\n',__name__)
 
     #check if quest is complete
-    appleInLocation = os.path.exists(os.path.join(CurrentLocation, "Apple.py"))
-    if (appleInLocation):
-        pass
+    applesInLocation = Search("Apple", CurrentLocation)
+    if (applesInLocation):
         #update the state and reimport
-
+        Update(State.__file__, "quest1complete","True")
+    
+    importlib.reload(State)
 
     if (not State.quest1complete):
         if (not State.quest1given):
-            Say("Hail Adventurer! I have not seen you in these parts before. Could you be the prophesied File Explorer?")
+            Say(__name__,"Hail Adventurer! I have not seen you in these parts before. Could you be the prophesied File Explorer?")
+
             options = ["I don\'t know, I just got here.",
             "Perhaps.",
             "I don\'t think so..."]
             Ask(options)
 
-            print('\n',__name__)
-            Say("I have been earning for the apple that is upon the tree for ages past. But alas, I am but lines of code in this world, unable to move! This Prophecy Neo Graphics (PNG) depicts a way to move things, but I cannot understand it. Perhaps you could help? Please bring me the apple!")
+            Say(__name__,"I have been earning for the apple that is upon the tree for ages past. But alas, I am but lines of code in this world, unable to move! This Prophecy Neo Graphics (PNG) depicts a way to move things, but I cannot understand it. Perhaps you could help? Please bring me the apple!")
             Show("poop.png")
+            Update(State.__file__, "quest1given","True")
+
         else:
-            Say("Please Explorer, bring me the apple!")
+            Say(__name__,"Do you want to see the PNG again?")
+
+            options = ["Yes, please.",
+            "No thank you."]
+            if (Ask(options)==1):
+                Say("Here you go.")
+                Show("poop.png")
+
+            Say(__name__,"Please Explorer, bring me the apple!")
+    else:
+        if (not State.quest1given):
+            Say(__name__,"Hail Adventurer!")
+            Say(__name__,"...")
+            Wait(2)
+            Say(__name__,"How... HOW DID YOU GET THE APPLE DOWN!?!?!? I have been here for ages pining for that apple. But alas, I am but lines of code in this world, unable to move. You must be the prophesised File Explorer!")
+        else:
+            Say(__name__,"...")
+            Wait(1)
+            Say(__name__,"IT IS TRUE. You are the Explorer.")
