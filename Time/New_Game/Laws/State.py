bff83722b25708ecd795103cce9dcf0488713f08
Isaaaca <isaaaca@gmail.com>
2020-04-11 15:13:23 +0800

    Add final game over message

--- Time/New_Game/Laws/State.py
+++ Time/New_Game/Laws/State.py
@@ -11,9 +11,19 @@ quest5complete = False
 quest6given = False
 quest6complete = False
 spokenToProphetOnLiberation = False
+prophet_liberated = False
+grandmaster_liberated = False
+poultry_seller_liberated = False
+quack_liberated = False
 
 def CanLiberate():
     for att in globals():
         if ("complete" in att) and (globals()[att]== False):
+            return False
+    return True
+
+def allLiberated():
+    for att in globals():
+        if ("liberated" in att) and (globals()[att]== False):
             return False
     return True
